fca646505bd8ad05a7b98fb4ec38347bc7af4ac2
Maria De Avelar Galvao Santos Lima <mde-avel@c2r7s9.42lisboa.com>
2024-10-05 14:03:56 +0100

    update ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,8 +4,8 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cout << RED << "Error! Please input only of expression." << RESET << std::endl;
-		std::cout << YELLOW << "Example: ./RPN \"Exprecion\"" << RESET << std::endl;
+		std::cout << RED << "Error! Please use an expression." << RESET << std::endl;
+		std::cout << YELLOW << "Example: ./RPN \"7 7 * 7 -\"" << RESET << std::endl;
 		return (1);
 	}
 	RPN::calculate(argv[1]);
